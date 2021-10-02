8853fedc44ea3c0ba0720c65a90dc9e4c15936e6
Uzzal Saha <55195197+uzzal623@users.noreply.github.com>
2021-10-03 00:55:07 +0600

    Create Staircase function
    Staircase function using c language.

---
+++ 2021/C/Algorithms/Staircase function.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int height;
+    scanf("%d",&height);
+    int stair = height - 1;
+    
+    for (int i = 0; i < height; ++i){
+        for (int j = 0; j < height; ++j){
+            if (j >= stair){
+                printf("#");
+            } else{
+                printf(" ");
+            }
+        }
+        stair -= 1;
+        printf("\n");
+    }    
+    return 0;
+}
